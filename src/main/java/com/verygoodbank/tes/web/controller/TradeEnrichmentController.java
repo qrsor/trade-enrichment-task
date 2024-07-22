2416de4fb6eaf586685d0595f2647d71f32383e4
qrsor <3127715+qrsor@users.noreply.github.com>
2024-07-22 07:53:51 +0200

    Basic response with logging

--- src/main/java/com/verygoodbank/tes/web/controller/TradeEnrichmentController.java
+++ src/main/java/com/verygoodbank/tes/web/controller/TradeEnrichmentController.java
@@ -1,15 +1,25 @@
 package com.verygoodbank.tes.web.controller;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api/v1")
 public class TradeEnrichmentController {
 
-    // @RequestMapping(value = "/enrich", method = RequestMethod.POST)
-    // TODO
+	Logger logger = LoggerFactory.getLogger(TradeEnrichmentController.class);
+
+	@RequestMapping(value = "/enrich", method = RequestMethod.POST)
+	public String enrich() {
+
+		logger.debug("Received enrich request");
+
+		return "OK";
+	}
 
 }
 
