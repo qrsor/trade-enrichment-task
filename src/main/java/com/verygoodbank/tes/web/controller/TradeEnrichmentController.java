29fc2a93f120b1b2cc67c9b3349f69aa4a851fda
qrsor <3127715+qrsor@users.noreply.github.com>
2024-07-22 07:54:21 +0200

    Controller accepts files and writes response as-is to a stream

--- src/main/java/com/verygoodbank/tes/web/controller/TradeEnrichmentController.java
+++ src/main/java/com/verygoodbank/tes/web/controller/TradeEnrichmentController.java
@@ -1,24 +1,55 @@
 package com.verygoodbank.tes.web.controller;
 
 
+import jakarta.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 
 @RestController
 @RequestMapping("api/v1")
-public class TradeEnrichmentController {
+public class TradeEnrichmentController
+{
 
 	Logger logger = LoggerFactory.getLogger(TradeEnrichmentController.class);
 
-	@RequestMapping(value = "/enrich", method = RequestMethod.POST)
-	public String enrich() {
+	@RequestMapping(value = "/enrich", method = RequestMethod.POST, produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
+	public void enrich(@RequestParam("file") MultipartFile file, HttpServletResponse response) throws IOException
+	{
+		try (var inputStream = file.getInputStream();
+				var reader = new BufferedReader(new InputStreamReader(inputStream));
+				var outStream = response.getOutputStream();)
+		{
+			String line;
+			reader.readLine(); //skip 1
+			outStream.write("date,product_name,currency,price\n".getBytes());
+			while ((line = reader.readLine()) != null)
+			{
+				outStream.write(processLine(line).getBytes());
+				outStream.write('\n');
+			}
+			response.setContentType(MediaType.APPLICATION_OCTET_STREAM.toString());
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+	}
 
-		logger.debug("Received enrich request");
-
-		return "OK";
+	private String processLine(String line)
+	{
+		return line;
 	}
 
 }
--- src/test/java/com/verygoodbank/tes/TradeEnrichmentServiceApplicationTests.java
+++ src/test/java/com/verygoodbank/tes/TradeEnrichmentServiceApplicationTests.java
@@ -1,5 +1,35 @@
 package com.verygoodbank.tes;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@SpringBootTest(
+		webEnvironment = SpringBootTest.WebEnvironment.MOCK,
+		classes = TradeEnrichmentServiceApplication.class)
+@AutoConfigureMockMvc
 class TradeEnrichmentServiceApplicationTests {
 
+	@Autowired
+	private MockMvc mvc;
+
+	@Test
+	public void enrichShouldReturnSampleFileWithoutError() throws Exception {
+		var tradeCsv = new ClassPathResource("trade.csv").getInputStream();
+		MockMultipartFile file = new MockMultipartFile("file", tradeCsv);
+
+		mvc.perform(MockMvcRequestBuilders.multipart("/api/v1/enrich").file(file).contentType(MediaType.MULTIPART_FORM_DATA_VALUE))
+				.andExpect(status().isOk())
+				.andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_OCTET_STREAM));
+	}
 }
